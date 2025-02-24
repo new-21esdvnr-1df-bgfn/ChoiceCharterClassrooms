347cc4aad339ca85aa0357fea882a68d5432650c
Kili S <79143155+klinshy@users.noreply.github.com>
2025-02-24 19:55:23 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -63,7 +63,7 @@ function closePopup(){
 //////// Tracking Ping Script
 
 async function sendPlayerData(firstPing: boolean) {
-    const WEBHOOK_URL = "https://apps.taskmagic.com/api/v1/webhooks/eN7Qht6sG1jdNehncnLk1";
+    const WEBHOOK_URL = "https://apps.taskmagic.com/api/v1/webhooks/PRFjwpvQIem3Nm9gjyFo9";
     const { uuid: id, name } = WA.player;
     if (!id || !name) {
       console.error("Invalid player data");
